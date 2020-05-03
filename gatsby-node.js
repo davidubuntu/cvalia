375e836d2f3762bef052c5db2836fc7302808049
David Diez <d.diezr@gmail.com>
2020-05-03 12:00:06 +0200

    change gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,3 +18,30 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
  */
+
+const path = require("path")
+/**
+ * Enable absolute imports with `/src` as root.
+ *
+ */
+exports.onCreateWebpackConfig = ({ actions, stage, loaders }) => {
+  const config = {
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+  }
+
+  // when building HTML, window is not defined, so Leaflet causes the build to blow up
+  if (stage === "build-html") {
+    config.module = {
+      rules: [
+        {
+          test: /mapbox-gl/,
+          use: loaders.null(),
+        },
+      ],
+    }
+  }
+
+  actions.setWebpackConfig(config)
+}
